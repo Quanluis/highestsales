ad85f7f0671551f04eea22f86fafe9a1bc6259b7
Quanluis <quanluis@protonmail.com>
2019-10-11 20:39:42 -0400

    Finished program still going to add code, realized this wasnt the best practice atm so will change logic later

--- main.cpp
+++ main.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
 using namespace std;
 
-float getSales();
-void validate();
+int* getSales();
+int validate();
+void findHighest();
 
 int main(){
 
-     getSales();
+        findHighest();
 
     return 0;
 }
 
-float getSales(){
+int* getSales(){
 
-        validate();
-        validate();
-        validate();
-        validate();
+        int sales1 = validate();
+        int sales2 = validate();
+        int sales3 = validate();
+        int sales4 = validate();
 
-        return 0;
+        static int arr[4] = {sales1, sales2, sales3, sales4};
+
+        return arr;
 }
 
 void findHighest(){
 
+
+        int * sales = getSales();
+
+        cout << sales[0] << sales[1] << sales[2] << sales[3] << endl;
+
+        if(sales[0] > sales[1] && sales[0] > sales[2] && sales[0] > sales[3]){
+            cout <<  sales[0] << " is the biggest.";
+        } else if (sales[1] > sales[0] && sales[1] > sales[2] && sales[1] > sales[3]){
+                cout <<  sales[1] << " is the biggest.";
+        }else if (sales[2] > sales[0] && sales[2] > sales[1] && sales[2] > sales[3]){
+                cout <<  sales[2] << " is the biggest.";
+        }else if (sales[3] > sales[0] && sales[3] > sales[2] && sales[3] > sales[1]){
+                cout <<  sales[3] << " is the biggest.";
+        }
+
+ 
 }
 
-void validate(){
+int validate(){
 
         int sales;
 
@@ -43,6 +62,7 @@ void validate(){
         } else {
                 cout << "Your sales are " << sales << endl;
         } 
+        return sales;
 }      
 
 
